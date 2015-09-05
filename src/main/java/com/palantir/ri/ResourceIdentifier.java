887ddc0fc5b8559bcf3404b5b3c078bf2ca1feaa
Jason Zhao <jasonz@palantir.com>
2015-09-04 17:00:00 -0700

    fixes #9 - add javadoc and javadoc building

--- src/main/java/com/palantir/ri/ResourceIdentifier.java
+++ src/main/java/com/palantir/ri/ResourceIdentifier.java
@@ -18,6 +18,24 @@ import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Defines a common format for wrapping existing unique identifiers to provide additional context. This class
+ * provides utility method: {@code #of(String)} to parse an existing identifier or the ability to generate new
+ * identifiers by using provided context with method {@code #of(String, String, String, String)}.
+ * <p>
+ * Resource identifier specification:
+ * <p>
+ * Resource Identifiers contain 4 components, prefixed by a format identifier ri and separated with periods:
+ * {@code ri.<service>.<instance>.<type>.<locator>}
+ * <ol>
+ * <li> Service: a string that represents the service (or application) that namespaces the rest of the identifier.
+ *      Must conform with regex pattern [a-z][a-z0-9\-]*
+ * <li> Instance: an optionally empty string that represents a specific service cluster, to allow disambiguation of
+ *      artifacts from different service clusters. Must conform to regex pattern ([a-z0-9][a-z0-9\-]*)?
+ * <li> Type: a service-specific resource type to namespace a group of locators. Must conform to regex pattern [a-z][a-z0-9\-]*
+ * <li> Locator: a string used to uniquely locate the specific resource. Must conform to regex pattern [a-zA-Z0-9\-\._]+
+ * </ol>
+ */
 public final class ResourceIdentifier {
     private static final String RID_CLASS = "ri";
     private static final String SEPARATOR = ".";
@@ -51,22 +69,50 @@ public final class ResourceIdentifier {
         this.locator = locator;
     }
 
+
+    /**
+     * Returns the service component
+     *
+     * @return the service component from the current identifier
+     */
     public String getService() {
         return service;
     }
 
+    /**
+     * Returns the instance component
+     *
+     * @return the instance component from the current identifier
+     */
     public String getInstance() {
         return instance;
     }
 
+    /**
+     * Returns the type component
+     *
+     * @return the type component from the current identifier
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Returns the locator component
+     *
+     * @return the locator component from the current identifier
+     */
     public String getLocator() {
         return locator;
     }
 
+    /**
+     * Returns a string representation of this ResourceIdentifier. The string representation
+     * follows the format specification using the "ri" header followed by the 4 components
+     * separated by periods.
+     *
+     * @return a string representation of this identifier
+     */
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder(RID_CLASS).append(SEPARATOR)
@@ -77,11 +123,26 @@ public final class ResourceIdentifier {
         return builder.toString();
     }
 
+    /**
+     * Returns the hash code value for identifier.  The hash code
+     * is calculated using the Java @{code {@link Objects#hash(Object...)} method
+     * over each of the 4 components.
+     *
+     * @return the hash code value for this identifier
+     */
     @Override
     public int hashCode() {
         return Objects.hash(service, instance, type, locator);
     }
 
+    /**
+     * Compares the specified object with this identifier for equality.  Returns
+     * {@code true} if and only if the specified object is also a resource identifier and
+     * contain exactly the same values for all 4 components.
+     *
+     * @param obj the object to be compared for equality with this identifier
+     * @return {@code true} if the specified object is equal to this identifier, {@code false} otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == this) {
@@ -97,10 +158,27 @@ public final class ResourceIdentifier {
                 Objects.equals(locator, other.locator);
     }
 
+    /**
+     * Checks if the input string is a valid resource identifier as defined in the specification
+     *
+     * @param rid the input string to be checked
+     * @return {@code true} if and only if the input satisfy the resource identifier specification,
+     *         {@code false} otherwise.
+     */
     public static boolean isValid(String rid) {
         return rid != null && SPEC_PATTERN.matcher(rid).matches();
     }
 
+    /**
+     * Generates a new resource identifier object from the input string. This method will validate
+     * that the input is a valid resource identifier string as defined by the specification prior
+     * to creating the object.
+     *
+     * @param rid the input string to be converted to a resource identifier
+     * @return a resource identifier object representing the input string
+     *
+     * @throws IllegalArgumentException if the input string is not a valid resource identifier
+     */
     public static ResourceIdentifier of(String rid) {
         if (rid != null) {
             Matcher matcher = SPEC_PATTERN.matcher(rid);
@@ -111,6 +189,19 @@ public final class ResourceIdentifier {
         throw new IllegalArgumentException("Illegal resource identifier format: " + rid);
     }
 
+
+    /**
+     * Generates a new resource identifier object from each of the 4 input components. Each component must
+     * satisfy the requirements as defined by the specification.
+     *
+     * @param service input representing the service component
+     * @param instance input representing the instance component
+     * @param type input representing the type component
+     * @param locator input representing the locator component
+     * @return a resource identifier object representing the input components
+     *
+     * @throws IllegalArgumentException if any of the inputs do not satisfy the resource identifier specification
+     */
     public static ResourceIdentifier of(String service, String instance, String type, String locator) {
         checkServiceIsValid(service);
         checkInstanceIsValid(instance);
